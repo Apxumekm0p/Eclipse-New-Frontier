c04c65969d34b07b0963aa0db6f6e6bbd2c2afa2
FoLZer <65248062+FoLZer@users.noreply.github.com>
2025-03-07 01:13:53 +0100

    Prepare shipyard files

--- Content.Shared/_Eclipse/CCvar/CCVar.Eclipse.cs
+++ Content.Shared/_Eclipse/CCvar/CCVar.Eclipse.cs
@@ -13,4 +13,25 @@ public sealed class EclipseCCVars
 
     public static readonly CVarDef<string> DiscordInteractAlertWebhook =
         CVarDef.Create("eclipse.discord_interact_alert_webhook", string.Empty, CVar.SERVERONLY);
+
+    /*
+    * Shipyard
+    */
+    /// <summary>
+    /// Whether the Self Shipyard is enabled.
+    /// </summary>
+    public static readonly CVarDef<bool> SelfShipyard =
+        CVarDef.Create("shuttle.self_shipyard", true, CVar.SERVERONLY);
+
+    /// <summary>
+    /// Constant save rate in spessos (gets added with shuttle.self_shipyard_percent_save_rate to calculate total)
+    /// </summary>
+    public static readonly CVarDef<int> SelfShipyardConstantSaveRate =
+        CVarDef.Create("shuttle.self_shipyard_constant_save_rate", 50000, CVar.SERVERONLY);
+
+    /// <summary>
+    /// Percent save rate in spessos (gets added with shuttle.self_shipyard_constant_save_rate to calculate total)
+    /// </summary>
+    public static readonly CVarDef<float> SelfShipyardPercentSaveRate =
+        CVarDef.Create("shuttle.self_shipyard_percent_save_rate", 1.05f, CVar.SERVERONLY);
 }
